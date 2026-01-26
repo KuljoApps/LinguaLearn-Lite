30f43b373ca3ba2a460999b00e5b3398734e3bdf
Kuljo Apps <kuljoapps@gmail.com>
2026-01-26 00:53:51 +0000

    Ok, przywróć poprzednią wersję, bardziej mi się podobała. W home screen

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -122,10 +122,11 @@ export default {
           '100%': { transform: 'translateX(0)', opacity: '1' },
         },
         'gradient-flow': {
-          '0%, 100%': { 'background-position': '0% 50%' },
+          '0%': { 'background-position': '0% 50%' },
           '25%': { 'background-position': '100% 0%' },
           '50%': { 'background-position': '100% 100%' },
           '75%': { 'background-position': '0% 100%' },
+          '100%': { 'background-position': '0% 50%' },
         },
       },
       animation: {
